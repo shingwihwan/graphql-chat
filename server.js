3c9bd42d26442eacc3879a4acd30f78ac5212d16
zamar <zerll9100@gmail.com>
2021-11-11 00:26:32 +0900

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'hello world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({
+      // options
+    })
+  ]
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
